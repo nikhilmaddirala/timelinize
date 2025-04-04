020c84a0bc54b7600cb97e7db050b61b2c425f12
Nikhil Maddirala <nikhil.maddirala@gmail.com>
2025-04-04 23:46:11 +0000

    Add setup script for Go and required libraries

---
+++ nikhil/setup.sh
@@ -0,0 +1,18 @@
+# Install Go
+GO="$(curl -s https://go.dev/dl/?mode=json | grep -o 'go.*.linux-amd64.tar.gz' | head -n 1 | tr -d '\r\n' )"
+wget "https://dl.google.com/go/$GO"
+sudo tar -C /usr/local -xzf "$GO"
+rm "$GO"
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+source ~/.bashrc
+
+# libvips
+sudo apt update
+sudo apt install -y libvips-dev
+
+# libheif
+sudo add-apt-repository ppa:ubuntuhandbook1/libheif
+sudo apt update
+sudo apt install libheif-dev libheif1 -y
+
+sudo apt install libsqlite3-dev
