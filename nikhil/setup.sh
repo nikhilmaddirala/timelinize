0104f9c80b75abc8a0bed0462b77218be41cd229
Nikhil Maddirala <nikhil.maddirala@gmail.com>
2025-04-05 04:53:36 +0000

    Update setup script to install libvips from source and add necessary dependencies

--- nikhil/setup.sh
+++ nikhil/setup.sh
@@ -8,7 +8,15 @@ source ~/.bashrc
 
 # libvips
 sudo apt update
-sudo apt install -y libvips-dev
+sudo apt install -y libvips-dev # TODO old version
+
+# libvips from source
+sudo apt update
+sudo apt install -y build-essential pkg-config libglib2.0-dev libexpat1-dev libjpeg-dev libtiff-dev libwebp-dev liborc-0.4-dev libpng-dev
+
+wget https://github.com/libvips/libvips/releases/download/v8.10.6/vips-8.10.6.tar.gz
+tar xf vips-8.10.6.tar.gz
+cd vips-8.10.6 && ./configure && make && sudo make install && sudo ldconfig
 
 # libheif
 sudo add-apt-repository ppa:ubuntuhandbook1/libheif
@@ -20,3 +28,5 @@ sudo apt install libsqlite3-dev
 sudo apt install ffmpeg
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# go run main.go
